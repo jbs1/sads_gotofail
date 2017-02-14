e939d4bacf77b465bb389ec654bdbe352e3f1b8c
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-02-14 12:05:52 +0100

    init impl

--- gotofail.cpp
+++ gotofail.cpp
@@ -8,10 +8,13 @@ typedef string SSLContext;
 typedef int SSLBuffer;
 
 
-struct SSLHashSHA1
+class SSLHashSHA1
 {
-	OSStatus update(string*, int*);
-	OSStatus final (string*, string*);
+	SSLHashSHA1();
+	~SSLHashSHA1();
+	public:
+		static OSStatus update(string*, int*);
+		static OSStatus final (string*, string*);
 };
 
 OSStatus SSLHashSHA1::update(string* ctx, int* ran){
@@ -23,25 +26,27 @@ OSStatus SSLHashSHA1::final(string* ctx, string* out){
 }
 
 void SSLFreeBuffer(string* shash){
-	cout << "freebuff:" << shash <<endl;
+	cout << "freebuff:" << *shash <<endl;
 }
 
 
 
 string signedHashes = "hashstuff";
 string hashCtx = "hashsontext";
+int serverRandom = 123832131;
+string hashOut = "hashouttext";
 
 static OSStatus SSLVerifySignedServerKeyExchange(
 	SSLContext *ctx, bool isRsa, SSLBuffer signedParams, uint8_t *signature, uint16_t signatureLen)
 {
 	OSStatus err;
 
-	if ((err = SSLHashSHA1.update(&hashCtx, &serverRandom)) != 0)
+	if ((err = SSLHashSHA1::update(&hashCtx, &serverRandom)) != 0)
 		goto fail;
-	if ((err = SSLHashSHA1.update(&hashCtx, &signedParams)) != 0)
+	if ((err = SSLHashSHA1::update(&hashCtx, &signedParams)) != 0)
 		goto fail;
 		goto fail;
-	if ((err = SSLHashSHA1.final(&hashCtx, &hashOut)) != 0)
+	if ((err = SSLHashSHA1::final(&hashCtx, &hashOut)) != 0)
 		goto fail;
 
 
@@ -56,7 +61,7 @@ int main(int argc, char const *argv[])
 	SSLContext ctx = "bla";
 	bool isrsa = true;
 	SSLBuffer sigpar= 1234;
-	uint8_t sig= 1234;
+	uint8_t sig= 12;
 	uint16_t siglen= 5678;
 
 	OSStatus result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);

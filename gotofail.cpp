6d9c0045ebe65f12567fd5d8b857c1122d608efc
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-02-21 21:47:51 +0100

    TASK 2 done: unittest

--- gotofail.cpp
+++ gotofail.cpp
@@ -7,11 +7,24 @@ typedef int OSStatus;
 typedef string SSLContext;
 typedef int SSLBuffer;
 
+//useless parameters
+SSLContext ctx = "bla";
+bool isrsa = true;
+uint8_t sig= 12;
+uint16_t siglen= 5678;
+string signedHashes = "hashstuff";
+string hashCtx = "hashsontext";
 
-//testing parameters
+
+//testing parameters (init to all nonfail)
 int serverRandom = 0; //nonzero == fail > gotofail for first sha1 methode
 SSLBuffer sigpar= 0; //nonzero == fail > gotofail for second sha1 methode
-string hashOut = "fail"; //"fail" == error condition > gotofail for third sha1 methode
+string hashOut = "nonfail"; //"fail" == error condition > gotofail for third sha1 methode
+
+
+//init result
+OSStatus result = 0;
+
 
 
 class SSLHashSHA1
@@ -24,10 +37,12 @@ class SSLHashSHA1
 };
 
 OSStatus SSLHashSHA1::update(string* ctx, int* ran){
+	//cout << "--------" << *ran<<endl;
 	return *ran;
 }
 
 OSStatus SSLHashSHA1::final(string* ctx, string* out){
+	//cout << "--------" << *out<<endl;
 	if ((*out).compare("fail")==0){
 		return 1;
 	} else {
@@ -40,14 +55,10 @@ void SSLFreeBuffer(string* shash){
 
 
 
-string signedHashes = "hashstuff";
-string hashCtx = "hashsontext";
-
-
-static OSStatus SSLVerifySignedServerKeyExchange(
-	SSLContext *ctx, bool isRsa, SSLBuffer signedParams, uint8_t *signature, uint16_t signatureLen)
+static OSStatus SSLVerifySignedServerKeyExchange(SSLContext *ctx, bool isRsa, SSLBuffer signedParams, uint8_t *signature, uint16_t signatureLen)
 {
-	OSStatus err;
+	OSStatus err=0;
+
 
 	if ((err = SSLHashSHA1::update(&hashCtx, &serverRandom)) != 0)
 		goto fail;
@@ -57,6 +68,7 @@ static OSStatus SSLVerifySignedServerKeyExchange(
 	if ((err = SSLHashSHA1::final(&hashCtx, &hashOut)) != 0)
 		goto fail;
 
+	cout << "nonfail" << endl;
 
 	fail:
 		SSLFreeBuffer(&signedHashes);
@@ -64,16 +76,42 @@ static OSStatus SSLVerifySignedServerKeyExchange(
 		return err;
 }
 
+void unittest(){
+	//all tests passing
+	cout << "-----nonfail-----" << endl;
+	cout<<SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen)<<endl;
+
+
+	//first test fail
+	cout << "-----firstfail-----" << endl;
+	serverRandom = 1;
+	cout << SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen)<<endl;
+	serverRandom = 0;
+
+	//second test fail
+	cout << "-----secondfail-----" << endl;
+	sigpar= 1;
+	cout<<SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen)<<endl;
+	sigpar= 0;
+
+	//third test fail
+	cout << "-----thirdfail-----" << endl;
+	hashOut = "fail";
+	cout<<SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen)<<endl;
+	hashOut = "nonfail";
+
+}
+
+
+
+
+
 int main(int argc, char const *argv[])
 {
-	SSLContext ctx = "bla";
-	bool isrsa = true;
-	uint8_t sig= 12;
-	uint16_t siglen= 5678;
 
-	OSStatus result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
-	cout << result <<endl;
-
+	//OSStatus result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
+	//cout << result <<endl;
+	unittest();
 
 	return 0;
 }

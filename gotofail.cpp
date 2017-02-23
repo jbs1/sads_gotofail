efc2882f0aa13c3bf51cbda0c92c8258f32a6a89
Jeremy Byron Schulz <j.schulz@jacobs-university.de>
2017-02-23 19:14:28 +0100

    task 5_6

--- gotofail.cpp
+++ gotofail.cpp
@@ -8,45 +8,45 @@ typedef int OSStatus;
 typedef string SSLContext;
 typedef int SSLBuffer;
 
-//useless parameters
+// useless parameters
 SSLContext ctx = "bla";
 bool isrsa = true;
-uint8_t sig= 12;
-uint16_t siglen= 5678;
+uint8_t sig = 12;
+uint16_t siglen = 5678;
 string signedHashes = "hashstuff";
 string hashCtx = "hashsontext";
 
 
-//testing parameters (init to all nonfail)
-int serverRandom = 0; //nonzero == fail > gotofail for first sha1 methode
-SSLBuffer sigpar= 0; //nonzero == fail > gotofail for second sha1 methode
-string hashOut = "nonfail"; //"fail" == error condition > gotofail for third sha1 methode
+// testing parameters (init to all nonfail)
+int serverRandom = 0;  // nonzero == fail > gotofail for first sha1 methode
+SSLBuffer sigpar = 0;  // nonzero == fail > gotofail for second sha1 methode
+string hashOut = "nonfail";  // "fail" == error condition > gotofail for third sha1 methode
 
 
-//init result
+// init result
 OSStatus result = 0;
 
 
 
 class SSLHashSHA1
 {
-	SSLHashSHA1();
-	~SSLHashSHA1();
-	public:
-		static OSStatus update(string*, int*);
-		static OSStatus final (string*, string*);
+    SSLHashSHA1();
+    ~SSLHashSHA1();
+ public:
+    static OSStatus update(string*, int*);
+    static OSStatus final(string*, string*);
 };
 
 OSStatus SSLHashSHA1::update(string* ctx, int* ran){
-	return *ran;
+    return *ran;
 }
 
 OSStatus SSLHashSHA1::final(string* ctx, string* out){
-	if ((*out).compare("fail")==0){
-		return 1;
-	} else {
-		return 0;
-	}
+    if ((*out).compare("fail") == 0){
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 void SSLFreeBuffer(string* shash){
@@ -56,76 +56,75 @@ void SSLFreeBuffer(string* shash){
 
 static OSStatus SSLVerifySignedServerKeyExchange(SSLContext *ctx, bool isRsa, SSLBuffer signedParams, uint8_t *signature, uint16_t signatureLen)
 {
-	OSStatus err=0;
+    OSStatus err = 0;
 
 
-	if ((err = SSLHashSHA1::update(&hashCtx, &serverRandom)) != 0)
-		goto fail;
-	if ((err = SSLHashSHA1::update(&hashCtx, &signedParams)) != 0)
-		goto fail;
-		goto fail;
-	if ((err = SSLHashSHA1::final(&hashCtx, &hashOut)) != 0)
-		goto fail;
+    if ((err = SSLHashSHA1::update(&hashCtx, &serverRandom)) != 0)
+        goto fail;
+    if ((err = SSLHashSHA1::update(&hashCtx, &signedParams)) != 0)
+        goto fail;
+        goto fail;
+    if ((err = SSLHashSHA1::final(&hashCtx, &hashOut)) != 0)
+        goto fail;
 
 
-	fail:
-		SSLFreeBuffer(&signedHashes);
-		SSLFreeBuffer(&hashCtx);
-		return err;
+    fail:
+        SSLFreeBuffer(&signedHashes);
+        SSLFreeBuffer(&hashCtx);
+        return err;
 }
 
 bool unittest(){
-	//all tests passing
-	cout << "TESTING nonfail" << endl;
-	result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
-	if(result != 0){
-		cout << "FAIL!" << endl;
-		return true;
-	}
+    // all tests passing
+    cout << "TESTING nonfail" << endl;
+    result = SSLVerifySignedServerKeyExchange(&ctx, isrsa, sigpar, &sig, siglen);
+    if (result != 0){
+        cout << "FAIL!" << endl;
+        return true;
+    }
 
 
-	//first test fail
-	cout << "TESTING first fail" << endl;
-	serverRandom = 1;
-	result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
-	serverRandom = 0;
-	if(result == 0){
-		cout << "FAIL!" << endl;
-		return true;
-	}
+    // first test fail
+    cout << "TESTING first fail" << endl;
+    serverRandom = 1;
+    result = SSLVerifySignedServerKeyExchange(&ctx, isrsa, sigpar, &sig, siglen);
+    serverRandom = 0;
+    if (result == 0){
+        cout << "FAIL!" << endl;
+        return true;
+    }
 
-	//second test fail
-	cout << "TESTING second fail" << endl;
-	sigpar = 1;
-	result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
-	sigpar = 0;
-	if(result == 0){
-		cout << "FAIL!" << endl;
-		return true;
-	}
+    // second test fail
+    cout << "TESTING second fail" << endl;
+    sigpar = 1;
+    result = SSLVerifySignedServerKeyExchange(&ctx, isrsa, sigpar, &sig, siglen);
+    sigpar = 0;
+    if (result == 0){
+        cout << "FAIL!" << endl;
+        return true;
+    }
 
-	//third test fail
-	cout << "TESTING third fail" << endl;
-	hashOut = "fail";
-	result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
-	hashOut = "nonfail";
-	if(result == 0){
-		cout << "FAIL!" << endl;
-		return true;
-	}
-	return false;
-
+    // third test fail
+    cout << "TESTING third fail" << endl;
+    hashOut = "fail";
+    result = SSLVerifySignedServerKeyExchange(&ctx, isrsa, sigpar, &sig, siglen);
+    hashOut = "nonfail";
+    if (result == 0){
+        cout << "FAIL!" << endl;
+        return true;
+    }
+    return false;
 }
 
 
 
 int main(int argc, char const *argv[])
 {
-	if (argc==2 && strcmp(argv[1],"testing")==0){
-		return unittest();
-	}
+    if (argc == 2 && strcmp(argv[1], "testing") == 0){
+        return unittest();
+    }
 
-	//OSStatus result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
+    // OSStatus result = SSLVerifySignedServerKeyExchange(&ctx,isrsa,sigpar,&sig,siglen);
 
-	return 0;
+    return 0;
 }
